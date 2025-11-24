ee16c87d8307dbb2d31874bd3c5a8392a85dc0d0
sammy cherbatji <C00312484@setu.ie>
2025-11-24 11:33:23 +0000

    test: slows down correctly

--- Tests/Physics_test.cpp
+++ Tests/Physics_test.cpp
@@ -12,3 +12,14 @@ TEST(PhysicsTest, ApplyThrust_IncreasesVelocityCorrectly)
     EXPECT_FLOAT_EQ(dx, 1.0f);
     EXPECT_FLOAT_EQ(dy, 0.0f);
 }
+
+// 2. TEST SLOWS DOWN CORRECTLY
+TEST(PhysicsTest, ApplyDrag_ReducesVelocityCorrectly)
+{
+    float dx = 10, dy = -10;
+
+    Physics::applyDrag(dx, dy, 0.5f);
+
+    EXPECT_FLOAT_EQ(dx, 5.0f);
+    EXPECT_FLOAT_EQ(dy, -5.0f);
+}
